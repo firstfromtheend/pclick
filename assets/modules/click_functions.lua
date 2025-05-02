190f61eb02dc6c5acd8f5a62834cb775bd41ebf3
firstfromtheend <tooxicroot@protonmail.com>
2025-05-02 21:50:04 +0300

    minor adjustments
    - 3 different ingredient for clicks
    - adjustments for data system

--- assets/modules/click_functions.lua
+++ assets/modules/click_functions.lua
@@ -2,21 +2,28 @@ local player_data = require('assets.modules.player_data')
 
 local CLICK_FUNC = {}
 
-CLICK_FUNC.button_animate = false
+CLICK_FUNC.button_animate = {
+    ['mushroom'] = false,
+    ['herbs'] = false,
+    ['flower'] = false
+}
 
-function CLICK_FUNC.add_point()
-    player_data.current_score = player_data.current_score + 1
+function CLICK_FUNC.add_point(node_name)
+    player_data.ingredients[node_name] = player_data.ingredients[node_name] + 
+    player_data.gathering_rates[node_name].current_income
 end
 
-function CLICK_FUNC.click_made()
-    CLICK_FUNC.add_point()
-    print("click_made")
+function CLICK_FUNC.make_upgrade(node_name)
+    -- add states for 0 level upgrade and max level upgrades
+    player_data.gathering_rates[node_name].level = player_data.gathering_rates[node_name].level + 1
+    player_data.gathering_rates[node_name].current_income = player_data.gathering_rates[node_name].base_rate * 
+    (player_data.gathering_rates[node_name].rate_multiplier ^ player_data.gathering_rates[node_name].level)
+    player_data.gathering_rates[node_name].price_for_upgrade = player_data.gathering_rates[node_name].base_price * 
+    (player_data.gathering_rates[node_name].price_multiplier ^ player_data.gathering_rates[node_name].level)
 end
-
-
 
 function CLICK_FUNC.get_current_score()
-    return player_data.current_score
+    return player_data.ingredients
 end
 
 return CLICK_FUNC
--- assets/modules/player_data.lua
+++ assets/modules/player_data.lua
@@ -3,24 +3,42 @@ local PLAYER_DATA = {}
 PLAYER_DATA.current_score = 0
 
 PLAYER_DATA.ingredients = {
-    herbs = 0,
-    mushrooms = 0,
-    flowers = 0
+    ['mushroom'] = 0,
+    ['herbs'] = 0,
+    ['flower'] = 0
 }
 
 PLAYER_DATA.gathering_rates= {
     -- Base gathering rates for each ingredient
-    herbs = {
-        base_rate = 1,
-        current_multiplier = 1
+    ['mushroom'] = {
+        base_rate = 0.5,
+        rate_multiplier = 1.07,
+        level = 1,
+        price_for_upgrade = 0,
+        base_price = 15,
+        price_multiplier = 1.15,
+        current_income = 0.5,
+        max_level = 100
     },
-    mushrooms = {
-        base_rate = 1,
-        current_multiplier = 1
+    ['herbs'] = {
+        base_rate = 0.1,
+        rate_multiplier = 1.07,
+        level = 0,
+        price_for_upgrade = 0,
+        base_price = 100,
+        price_multiplier = 1.15,
+        current_income = 0,
+        max_level = 100
     },
-    flowers = {
-        base_rate = 1,
-        current_multiplier = 1
+    ['flower'] = {
+        base_rate = 4,
+        rate_multiplier = 1.07,
+        level = 0,
+        price_for_upgrade = 0,
+        base_price = 500,
+        price_multiplier = 1.15,
+        current_income = 0,
+        max_level = 100
     }
 }
 
